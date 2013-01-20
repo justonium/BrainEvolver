abec7027ffc3bfeb486a7810fb94eedab1e1fdf9
Justin <justinorthrop@gmail.com>
2013-01-20 16:01:39 -0500

    __init__ empty

--- BrainEvolver/src/Tests/__init__.py
+++ BrainEvolver/src/Tests/__init__.py
@@ -4,14 +4,3 @@ Created on Jan 15, 2013
 @author: Justin
 '''
 
-from Brain import *
-
-brain = createEmpty([], [])
-#nnode = NeuronNode(leafNeuronNode, leafNeuronNode, False)
-#nnode.brain = brain
-#brain.seed.node = nnode
-#list(brain.seed.outSynapses)[0].node = SynapseNode(leafSynapseNode, leafSynapseNode, False)
-
-brain.elapseTime(1.0)
-
-brain2 = brain.spawn()
