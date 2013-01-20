5dc6860270f98fcf2a50d12d76e1b39b2032f20a
Justin <justinorthrop@gmail.com>
2013-01-20 16:01:07 -0500

    BrainTest icon fixed

---
+++ BrainEvolver/src/Tests/BrainTest.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 17, 2013
+
+@author: Justin
+'''
+
+from Brain import *
+
+brain = createEmpty([], [])
+#nnode = NeuronNode(leafNeuronNode, leafNeuronNode, False)
+#nnode.brain = brain
+#brain.seed.node = nnode
+#list(brain.seed.outSynapses)[0].node = SynapseNode(leafSynapseNode, leafSynapseNode, False)
+
+brain.elapseTime(1.0)
+
+brain2 = brain.spawn()
