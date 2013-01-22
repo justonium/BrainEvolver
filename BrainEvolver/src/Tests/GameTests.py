4109ebbdc20ea3ecc268c30ab7267ff97d9075b9
ciscprocess <ciscprocess@gmail.com>
2013-01-21 21:15:00 -0500

    ArrayInterface test code

--- BrainEvolver/src/Tests/GameTests.py
+++ BrainEvolver/src/Tests/GameTests.py
@@ -5,7 +5,13 @@ Created on Jan 20, 2013
 '''
 
 from World.World import World
+from Body.ArrayInterface import ArrayInterface
 
 if __name__ == '__main__':
-    world = World((2000, 2000))
-    world.simulate()
+    #world = World((2000, 2000))
+    #world.simulate()
+    ai = ArrayInterface([('hai', 10), ('bai', 5)])
+    ai.hai = range(10)
+    ai.bai = range(5) 
+    
+    varg = ai.hai
